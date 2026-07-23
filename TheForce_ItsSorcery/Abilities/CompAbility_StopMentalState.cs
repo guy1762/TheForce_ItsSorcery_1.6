f23b286f0ff319d3f9782e7547f2d153b184b49f
guy1762 <88149304+guy1762@users.noreply.github.com>
2026-07-23 15:04:22 -0700

    readded StopMentalState

---
+++ TheForce_ItsSorcery/Abilities/CompAbility_StopMentalState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using TheForce_ItsSorcery.Abilities;
+using Verse;
+using Verse.AI;
+
+namespace TheForce_ItsSorcery.Abilities
+{
+    internal class CompAbility_StopMentalState : CompAbilityEffect
+    {
+        public new CompProperties_StopMentalState Props => (CompProperties_StopMentalState)props;
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+            if (target.Thing is Pawn && target.Pawn.InMentalState)
+            {
+                target.Pawn.MentalState.RecoverFromState();
+                target.Pawn.jobs.EndCurrentJob(JobCondition.InterruptForced);
+            }
+        }
+    }
+
+
+
+
+    public class CompProperties_StopMentalState : CompProperties_AbilityEffect
+    {
+        public CompProperties_StopMentalState()
+        {
+            compClass = typeof(CompAbility_StopMentalState);
+        }
+    }
+
+}
